d634dde7f6449d3e681c021afbc3e79155d45385
Raider144 <lawrence.isaac970@gmail.com>
2021-07-06 12:16:44 -0600

    added method endpoints as follows "get all threads", "get threadById", "create thread", "create post on thread", "delete", "deleteById"

--- server.js
+++ server.js
@@ -15,17 +15,18 @@ app.get("/thread",(req,res)=>{
     res.setHeader("Content-Type","application/json");
     console.log("getting all threads")
 
-    Thread.find(function(err,threads){
+    Thread.find({}, (err,thread) => {
         if(err){
             console.log("there was an error finding all threads")
             res.status(500).send(
                 JSON.stringify({
-                    message: `unable to find thread ${req.params.id}`,
+                    message: `unable to find threads ${req.params.id}`,
                     error: err,
                 })
             )
+            return;
         }
-        res.status(200).send(JSON.stringify(threads));
+        res.status(200).send(JSON.stringify(thread));
     });
 });
 
@@ -33,19 +34,91 @@ app.get("/thread",(req,res)=>{
 app.get("/thread/:id",(req,res)=>{
     res.setHeader("Content-Type","application/json");
     console.log(`getting a thread by id ${req.params.id}`)
-
+    
+    Thread.findById(req.params.id, (err, thread)=> {
+        if(err != null){
+            console.log("there was an error finding all threads")
+            res.status(500).send(
+                JSON.stringify({
+                    message: `unable to find threads ${req.params.id}`,
+                    error: err,
+                })
+            )
+            return;
+        } else if (thread === null){
+            res.status(400).json({
+                error: err,
+                message: "could not find thread",
+            });
+            return;
+        };
+        res.status(200).send(JSON.stringify(thread));
+    });
 });
 
 // POST /thread
 app.post("/thread",(req,res)=>{
     res.setHeader("Content-Type","application/json");
     console.log("posting a new thread")
+
+    let newThread = {
+        name: req.body.name || "",
+        author: req.body.author || "",
+        description: req.body.description || "",
+        category: req.body.category || "",
+        posts: [],
+    }
+
+    Thread.create(newThread, (err,thread) => {
+        if(err){
+            console.log("there was an error finding all threads")
+            res.status(500).send(
+                JSON.stringify({
+                    message: `unable to find threads ${req.params.id}`,
+                    error: err,
+                })
+            )
+            return;
+        }
+        res.status(200).send(JSON.stringify(thread));
+    });
+
 });
 
 // POST /post;
 app.post("/post",(req,res)=>{
     res.setHeader("Content-Type","application/json");
     console.log("posting a new post")
+
+    let newPost = {
+        author: req.body.author || "",
+        body: req.body.body || "",
+        thread_id: req.body.thread_id,
+    };
+
+    console.log(newPost);
+
+    Thread.findByIdAndUpdate(req.body.thread_id, {$push:{posts: newPost}}, {new:true,}, (err, thread)=>{
+        
+        if(err != null){
+            console.log("there was an error finding all threads")
+            res.status(500).send(
+                JSON.stringify({
+                    message: `unable to find threads ${req.params.id}`,
+                    error: err,
+                })
+            )
+            return;
+        } else if (thread === null){
+            res.status(400).json({
+                error: err,
+                message: "could not find thread",
+            });
+            return;
+        }
+        res.status(200).json(thread.posts[thread.posts.length -1]);
+
+    });
 });
 
 // DELETE /thread/:id
@@ -57,7 +130,49 @@ app.delete("/thread/:id",(req,res)=>{
 // DELETE /post/:thread_id/:post_id
 app.delete("/thread/:thread_id/:post_id",(req,res)=>{
     res.setHeader("Content-Type","application/json");
-    console.log(`deleting post in thread by id ${req.params.post_id}`)
+    console.log(`deleting post with id, ${req.params.post_id} in thread with id ${req.params.thread_id}`)
+    Thread.findByIdAndUpdate(
+        req.params.thread_id,
+        {
+            $pull:{
+                posts:{
+                    _id: req.params.post_id,
+                },
+            },
+        },
+        (err, thread) => {
+            if (err != null){
+                res.status(500).json({
+                    error: err,
+                    message: "could not delete post",
+                })
+                return;
+            } else if (thread === null){
+                res.status(404).json({
+                    error: err,
+                    message: "could not find thread"
+                })
+                return;
+            }
+
+            //find the post that was deleted
+            let post;
+            thread.posts.forEach((e)=>{
+                if(e._id == req.params.post_id){
+                    post = e;
+                }
+            });
+            // if you cant find it return 404
+            if(post == undefined){
+                res.status(404).json({
+                    error: err,
+                    message: "could not find post",
+                });
+                return;
+            }
+            res.status(200).json(post);
+        }
+    )
 });
 
 
