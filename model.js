64d49a9c1079e66bad7095d86bd83ea118fa49d7
Raider144 <lawrence.isaac970@gmail.com>
2021-07-06 12:15:30 -0600

    added postSchema and threadSchema

--- model.js
+++ model.js
@@ -1,20 +1,40 @@
 // this file is where all the data[types] go
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const threadSchema = mongoose.Schema({ // this is a schema. it contains all datatypes
-    name: String, //headline
-    author: String,
-    description: String,
-    category: String,
-    
-});
+
+const postSchema = mongoose.Schema(
+    {
+        author: String,
+        body: String,
+        thread_id: {type: mongoose.Schema.Types.ObjectId, ref: "Thread"},
+    },
+
+    { // passing in options
+        timestamps: true
+    }
+
+);
+
+const threadSchema = mongoose.Schema(
+    { // this is a schema. it contains all datatypes
+        name: String, //headline
+        author: String,
+        description: String,
+        category: String,
+        posts: [postSchema]
+    },
+    { // passing in options
+        timestamps: true
+    }
+);
 
 
 const Thread = mongoose.model("Thread", threadSchema); // setting the variable Todo to the todoSchema and naming it "Todo"
-
+const Post = mongoose.model("Post", postSchema);
 
 // let store = {}; depricated
 
 module.exports = {
     Thread,
+    Post,
 }
